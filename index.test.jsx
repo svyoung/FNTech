ac62820d018fd0b70270d007251aab6e56f60de9
Sam Young <truong.vee@gmail.com>
2023-10-23 14:10:58 -0700

    cleanup

--- index.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import index from './index';
-
-test('renders learn react link', () => {
-  render(<index />);
-  const linkElement = screen.getByText(/FNTech/i);
-  expect(linkElement).toBeInTheDocument();
-});
